ae526ee0969532af97cf204511f5595b3183fa7b
juanestebanalarcon <=>
2022-02-04 23:59:52 -0500

    Actualización --

--- test-python-mysql/test-python/test_5.py
+++ test-python-mysql/test-python/test_5.py
@@ -1,3 +1,56 @@
 
 # 5) ordenar los terceros que se tienen en el archivo data.py por nombre y 
 # agregar dentro de cada tercero la compañia a la que pertenece
+from data import Data as d 
+terceros =d.thirds()
+companies = d.get_companies()
+InfoCompanies = {}
+InfoBranches=[]
+InfoTerceros=[]
+branch=d.get_branch()
+for index in companies:
+    for k in index:
+        for i in k.get("branches"):
+            for llave in branch:
+                    if llave.get(i):
+                       InfoBranches.append({"name":llave.get("name"),"address":i.get("address")})
+                    else: 
+                        continue 
+        InfoCompanies.append({"name":k.get("name"),"nit":k.get("nit"),"branches":InfoBranches})
+for t in terceros:
+    for s in t:
+        for r in t.get("companyid"):
+            for k in InfoCompanies:
+                if k.get(r):
+                    InfoTerceros.append({
+    "billAddress1": s.get("billaddress1"),
+    "cellPhone": s.get("cellphone"),
+    "cityName": s.get("cityname"),
+    "companyid": k.get(r),
+    "email": s.get("email"),
+    "firstname": s.get("firstname"),
+    "identificationDv": s.get("identificationDv"),
+    "identificationNumber": s.get("identificationNumber"),
+    "lastname": s.get("lastname"),
+    "maidenname": s.get("maidenname"), 
+    "phone": s.get("phone"),
+    "state": s.get("state"),
+    "tradename":s.get("tradename")
+})
+ordered_by_name = sorted(InfoTerceros, key=lambda x: x["firstname"])
+
+'''
+"billAddress1": ,
+"cellPhone": ,
+"cityName": ,
+"companyid": ,
+"email": ,
+"firstname": ,
+"identificationDv": ,
+"identificationNumber": ,
+"lastname": ,
+"maidenname": , 
+"phone": "",
+"state": ,
+"tradename":
+'''
--- test-python-mysql/test-python/test_9.py
+++ test-python-mysql/test-python/test_9.py
@@ -3,3 +3,17 @@
 # en caso de que esté lo tenga. 
 # El resultado del ordenando debe ser así, en la parte inicial los items 
 # que no tienen color y en la parte final los que si tienen color
+from data import Data as d 
+items = d.items()
+colors = d.colors()
+InfoItems=[]
+for l in items:
+    for j in l:
+        for clave in colors:
+            if j.get("color") !="":
+                if clave.get(j.get("color")):
+                    colorAux=clave.get(j.get("color"))
+                InfoItems.append({"code":j.get("code"),"name":j.get("name"),"color":colorAux.get("ColorName")})
+            else:
+                InfoItems.append({"code":j.get("code"),"name":j.get("name"),"color":""})
+ordered_items=sorted(InfoItems, key=lambda x:x["name"])
